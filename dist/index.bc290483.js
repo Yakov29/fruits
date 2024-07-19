2a52db1ccd3012489f040cb9763fcf183b9b4dd1
FasimanYT <108018683+Yakov29@users.noreply.github.com>
2024-07-19 13:16:17 +0300

    Delete dist directory

--- dist/index.bc290483.js
+++
@@ -1,7 +0,0 @@
-const e=e=>e.map(e=>`
-    <li>
-      <h2 class="name">${e.name}</h2>
-      <span class="price">${e.price}</span>
-      <img src="${e.photo}" alt="image">
-    </li>`).join(""),t=document.querySelector(".list"),o=()=>fetch("http://localhost:1234/fruits").then(e=>e.json()).then(o=>{console.log(o),t.insertAdjacentHTML("beforeend",e(o))});document.querySelector(".form"),document.querySelector(".name"),document.querySelector(".price"),document.querySelector(".photo");const n=document.querySelector(".submit"),r=(e,t,o)=>fetch("http://localhost:1234/fruits",{method:"POST",body:json.stringify({name:e,price:t,photo:o})});n.addEventListener("submit",e=>{e.preventDefault();let t=e.target.elements.fruitname,n=e.target.elements.fruitprice,c=e.target.elements.fruitphoto;console.log(t),r(t,n,c),o()}),o();
-//# sourceMappingURL=index.bc290483.js.map
